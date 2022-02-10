dd1cd0e6ab365e0222e8465a2dff8fc183eb285b
tom1299 <tom1299@jeeatwork.com>
2022-02-10 14:50:38 +0100

    Add first tdd test case

--- primes/calculate_primes.py
+++ primes/calculate_primes.py
@@ -1,7 +1,7 @@
 """
 Calculation of prime numbers
 """
-def calcualte_primes(start: 0, end: 100):
+def calcualte_primes(start=0, end=100):
     """
     Calculates all primes from start to end
     :param start: The number to start from
---
+++ tests/calculate_primes_test.py
@@ -0,0 +1,7 @@
+import unittest
+from primes.calculate_primes import calcualte_primes
+
+
+class CalculatePrimesTests(unittest.TestCase):
+    def test_calculate_primes(self):
+        self.assertListEqual(calcualte_primes(start=1, end=10), [1, 2, 3, 5, 7])
