80469f3617e9ef98e72bda786719011328370299
tom1299 <tom1299@jeeatwork.com>
2022-02-10 14:41:30 +0100

    Implement dummy prime method

--- primes/hello_world.py
+++ primes/hello_world.py
@@ -1,5 +1,11 @@
-"""Writes a sentence to sdtout
-
-Writes 'This line will be printed.' to stdout.
 """
-print("This line will be printed.")
+Calculation of prime numbers
+"""
+def calcualte_primes(start: 0, end: 100):
+    """
+    Calculates all primes from start to end
+    :param start: The number to start from
+    :param end: The number to end
+    :return: The primes between start and end
+    """
+    print(start + end)
