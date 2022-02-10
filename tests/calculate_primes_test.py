f3bed6dfc673ecc4760d71352ac78a242c61b244
tom1299 <tom1299@jeeatwork.com>
2022-02-10 15:31:02 +0100

    Fix prime numbers in test

--- tests/calculate_primes_test.py
+++ tests/calculate_primes_test.py
@@ -4,4 +4,4 @@ from primes.calculate_primes import calcualte_primes
 
 class CalculatePrimesTests(unittest.TestCase):
     def test_calculate_primes(self):
-        self.assertListEqual(calcualte_primes(start=1, end=10), [1, 2, 3, 5, 7])
+        self.assertListEqual(calcualte_primes(start=1, end=10), [2, 3, 5, 7])
