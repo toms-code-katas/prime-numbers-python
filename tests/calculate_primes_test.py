ec1510d64ff493060172d08491888e6f463e57d2
tom1299 <tom1299@jeeatwork.com>
2022-02-11 08:57:43 +0100

    Closes #2; Add first tdd test using pytest

--- tests/calculate_primes_test.py
+++ tests/calculate_primes_test.py
@@ -3,5 +3,6 @@ from primes.calculate_primes import calcualte_primes
 
 
 class CalculatePrimesTests(unittest.TestCase):
+
     def test_calculate_primes(self):
         self.assertListEqual(calcualte_primes(start=1, end=10), [2, 3, 5, 7])
