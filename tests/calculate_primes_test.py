6e3205219cff28e33f0dd870734bf9b693ea657e
tom1299 <tom1299@jeeatwork.com>
2022-02-21 08:56:40 +0100

    Add test for calculating primes up to 100

--- tests/calculate_primes_test.py
+++ tests/calculate_primes_test.py
@@ -4,5 +4,10 @@ from primes.calculate_primes import calculate_primes
 
 class CalculatePrimesTests(unittest.TestCase):
 
-    def test_calculate_primes(self):
+    def test_calculate_primes_to_10(self):
         self.assertListEqual(calculate_primes(start=1, stop=10), [2, 3, 5, 7])
+
+    def test_calculate_primes_to_100(self):
+        self.assertListEqual(calculate_primes(start=1, stop=100),
+                             [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+                              89, 97])
