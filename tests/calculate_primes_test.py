bb3869075061170e7de644527683ebd0df744671
tom1299 <tom1299@jeeatwork.com>
2022-03-14 16:29:38 +0100

    Replace assertEuqalList with assertEqual

--- tests/calculate_primes_test.py
+++ tests/calculate_primes_test.py
@@ -5,12 +5,12 @@ from primes.calculate_primes import calculate_primes, InvalidRangeException
 class CalculatePrimesTests(unittest.TestCase):
 
     def test_calculate_primes_to_10(self):
-        self.assertListEqual(calculate_primes(start=1, stop=10), [2, 3, 5, 7])
+        self.assertEqual(calculate_primes(start=1, stop=10), [2, 3, 5, 7])
 
     def test_calculate_primes_to_100(self):
-        self.assertListEqual(calculate_primes(start=1, stop=100),
-                             [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
-                              47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+        self.assertEqual(calculate_primes(start=1, stop=100),
+                                 [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+                                  47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
 
     def test_calculate_primes_for_negative_ranges(self):
         self.assertRaises(InvalidRangeException, calculate_primes, -100, 0)
